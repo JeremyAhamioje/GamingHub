f0c646c4c0c42c2463dc333e8240769764fd5c52
jeremy Ahamioje <ahamiojejeremy@gmail.com>
2025-05-11 05:11:01 +0100

    Add chunk size configuration to Vite build

--- gaminghubcode/vite.config.js
+++ gaminghubcode/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'], // You can add other libraries as well
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust the chunk size warning limit to 1000 KB
+  },
 })
